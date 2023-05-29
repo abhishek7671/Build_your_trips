438a65f1103824c9084a2901f4742094867c1e13
Abhishek <abhisheksuda201@gmail.com>
2023-05-29 12:27:50 +0530

    changes

--- django_service/urls.py
+++ django_service/urls.py
@@ -1,14 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_jwt.blacklist.views import BlacklistView
-from rest_framework_jwt.views import obtain_jwt_token
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
     path('',include('apptrip.urls')),
-    path("auth/logout/", BlacklistView.as_view({"post": "create"}))
+    
 
 ]
